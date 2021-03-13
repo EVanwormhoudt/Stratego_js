58d0d8c16d55eebf82d1c52209b55ad4ccbd6f68
SheronBIS <milliens17formation@outlook.com>
2021-03-13 12:34:14 +0100

    Ajout de commentaires explicatifs dans le fichier main.js + ajout d'un mot oublié dans accueil.html

--- src/main.js
+++ src/main.js
@@ -52,31 +52,38 @@ io.use(sharedsession(session, {
     autoSave: true
 }));
 
-//get
+// redirige vers la page d'accueil
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/Front/Html/accueil.html');
     let sessionData = req.session;
-
 });
 
+// redirige vers la page de connexion si l'URL contient '/login'
 app.get("/login", (req, res) => {
     res.sendFile(__dirname + '/Front/Html/login.html');
 });
 
+// redirige vers la page d'enregistrement si l'URL contient '/register'
 app.get("/register", (req, res) => {
     res.sendFile(__dirname + '/Front/Html/register.html');
 });
 
+// redirige vers la page de deconnexion si l'URL contient '/logout'
 app.get('/logout', (req,res) => {
     req.session = null;
     res.redirect('/');
 });
+
+// redirige vers la page d'attente si l'URL contient '/waitingRoom'
 app.get('/waitingRoom', (req,res) => {
     res.sendFile(__dirname + '/Front/Html/salleAttente.html');
 });
+
 /******************/
 
+// Directement après la connexion au serveur :
 io.on('connection', (socket) => {
+
     socket.on("register", (info) => {
         let sql = "INSERT INTO users VALUES (default,?,?,?)";
         con.query(sql, [info[0], info[1],info[2]], (err, res)=> {
@@ -84,8 +91,8 @@ io.on('connection', (socket) => {
             console.log("personne ajouté")
         });
     });
-    socket.on("login",(info)=>{
 
+    socket.on("login",(info)=>{
         let sql = "SELECT id, username FROM users WHERE username = ? and password = ?";
         con.query(sql, [info[0], info[1]], (err, res) => {
             if(err) throw err;
@@ -122,8 +129,6 @@ function NumClientsInRoom(namespace, room) {
     var clients = io.nsps[namespace].adapter.rooms[room];
     return Object.keys(clients).length;
 }
-
-
 
 
 /******************/
